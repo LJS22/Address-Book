fb67e962f79d41dba173e1243e83e9a4800f7ca8
LJS22 <LJS22@users.noreply.github.com>
2020-07-28 13:55:17 +0100

    Updates

---
+++ precache-manifest.197d1466521e04f0e615f276326cab7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a748081877d867d0adc552c35efcd8ed",
+    "url": "/Address-Book/index.html"
+  },
+  {
+    "revision": "bb98ef76a043c000c064",
+    "url": "/Address-Book/static/css/main.bd1d08e5.chunk.css"
+  },
+  {
+    "revision": "01ceadbf703c52b1ed95",
+    "url": "/Address-Book/static/js/2.353c62d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Address-Book/static/js/2.353c62d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb98ef76a043c000c064",
+    "url": "/Address-Book/static/js/main.661ac925.chunk.js"
+  },
+  {
+    "revision": "dd52a08a1ca954bcdb76",
+    "url": "/Address-Book/static/js/runtime-main.5ff1c31c.js"
+  }
+]);
